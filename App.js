c4c95a1c44ae49b57ec4a9d8dcf605c71f606d7f
tylershienlim <tylershienlim@gmail.com>
2023-09-21 03:10:57 +1000

    initial get started screen
    get started screen created. just need to be aligned properly

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
+      <View style={top.container}>
+        <Image
+          source={require('./assets/csiro-logo.png')}
+          style={top.image}
+        />
+        <Text style={top.text}> 
+          Protect Your Data
+        </Text>
+      </View>
+      <View style={middle.container} >
+      </View>
+      <View style={bottom.container}>
+        <View style={bottom.buttonCover}>
+          <Button 
+            title="Get Started"
+            color="white"
+            onPress={() => Alert.alert("Get Started Pressed")}
+            style={bottom.button}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,3 +37,58 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const top = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    borderWidth: 2,
+  },
+  image: {
+    height: 112.5,
+    position: "relative",
+    width: 112.5,
+    margin: 25,
+  },
+  text: {
+    color: "rgb(0, 175, 219)",
+    fontSize: 25,
+  },
+})
+
+const middle = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+    height: 54,
+    width: '100%',
+    margin: 25,
+    borderWidth: 2,
+  }
+})
+
+const bottom = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 294,
+    width: 390,
+    borderWidth: 2,
+  },
+  buttonCover: {
+    backgroundColor: "rgb(0, 175, 219)",
+    borderRadius: 20,
+  },
+  button: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+    justifyContent: "center",
+    padding: 0,
+    position: "relative",
+  }
+})
